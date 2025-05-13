58f1781bb7686cb8ecc108bc80d161cefed7ce4f
francois <francois.lemerrer@pandata.ninja>
2025-05-13 11:12:31 +0200

    Add DAO Test Class

---
+++ src/test/java/fr/eni/zoo/dao/MockAnimalDAOTest.java
@@ -0,0 +1,75 @@
+package fr.eni.zoo.dao;
+
+import fr.eni.zoo.models.Animal;
+import fr.eni.zoo.models.AnimalType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MockAnimalDAOTest {
+
+	MockAnimalDAO animalDao = new MockAnimalDAO();
+
+	private static Animal lionMale;
+	private static Animal monkeyFemale;
+	private static Animal girafeMale;
+	private static Animal elephantFemale;
+	private static Animal unknown;
+
+
+	@BeforeAll
+	static void init() {
+		lionMale = new Animal(0, "Simba", false, AnimalType.LION);
+		monkeyFemale = new Animal(0, "Coco", true, AnimalType.MONKEY);
+		girafeMale = new Animal(0, "Gerald", false, AnimalType.GIRAFE);
+		elephantFemale = new Animal(0, "dumbo", true, AnimalType.ELEPHANT);
+		unknown = new Animal(100, "INCONNU", false, AnimalType.LION);
+	}
+
+	@Test
+	void insertAnimalShouldPopulateDAOAnimalsListWithAnimals() {
+		animalDao.insertAnimal(lionMale);
+		animalDao.insertAnimal(monkeyFemale);
+		animalDao.insertAnimal(girafeMale);
+		animalDao.insertAnimal(elephantFemale);
+		animalDao.insertAnimal(unknown);
+		for (Animal a : animalDao.animals) {
+			assertInstanceOf(Animal.class, a);
+		}
+	}
+
+	@Test
+	void updateAnimalShouldChangeTargetInDAOAnimalsList() throws DAOException {
+		String updatedName = "Moufassa";
+		lionMale.setName(updatedName);
+		animalDao.updateAnimal(lionMale);
+		assertEquals("Moufassa", animalDao.findById(1).getName());
+	}
+
+	@Test
+	void getAnimalByIdShouldReturnTargetAnimal() throws DAOException {
+		String expectedName = "Gerald";
+		assertEquals("Gerald", animalDao.findById(3).getName());
+	}
+
+	@Test
+	void getAnimalByIdGivenIncorrectIdShouldThrowDAOException() throws DAOException {
+		assertThrows(DAOException.class, () -> animalDao.findById(1000));
+	}
+
+	@Test
+	void findAllShouldReturnAllAnimalsFromDAOAnimalsList() {
+		List<Animal> animals = animalDao.findAll();
+			assertEquals(4, animals.size());
+		}
+
+	@Test
+	void deleteAnimalShouldDeleteAnimalFromDAOAnimalsList() throws DAOException {
+		animalDao.deleteAnimal(unknown);
+		assertThrows(DAOException.class, () -> animalDao.findById(100));
+	}
+}
+
---
+++ src/test/java/fr/eni/zoo/models/AnimalTest.java
@@ -0,0 +1,28 @@
+package fr.eni.zoo.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AnimalTest {
+
+    public static Animal animal;
+
+    @Test
+    public void defaultAnimalConstructorShouldNotReturnNullObject() {
+        animal = new Animal();
+        assertNotNull(animal);
+    }
+
+    @Test
+    public void animalConstructorShouldReturnInstanceWithGivenAttributes() {
+        Animal expectedAnimal = new Animal();
+        expectedAnimal.setId(1);
+        expectedAnimal.setName("Simba");
+        expectedAnimal.setFemale(false);
+        expectedAnimal.setAnimalType(AnimalType.LION);
+        animal = new Animal(1, "Simba", false, AnimalType.LION);
+        Assertions.assertEquals(expectedAnimal, animal);
+    }
+
+}
